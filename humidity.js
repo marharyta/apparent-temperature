3043ab4da1ff3e31913318769063ed5a94c6c17c
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-09-30 22:07:53 +0300

    Add humidity

---
+++ humidity.js
@@ -0,0 +1,12 @@
+// vapor pressure
+// Td - dewpoint temperature
+
+// e = 6.11 * 10^(7.5*Td / (237.3 + Td))
+
+
+// saturation vapor pressure in range 20-50C
+// Psat = 0.646 + 0.0555*T + 7.1 * 10^-5 * T^3
+// T - ambient temperaure
+
+// relative humidity 
+// rh = 
