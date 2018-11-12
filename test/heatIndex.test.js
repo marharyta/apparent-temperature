d2bc4978ded6e068ef86bbe4fc88c48ff80cd84d
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-11-12 09:28:11 +0200

    Relocate heat index tests

--- test/heatIndex.test.js
+++ test/heatIndex.test.js
@@ -1,4 +1,5 @@
 const calculateHeatIndex = require('../apparentTemperature/heatIndex');
+const heatIndex = require('../apparentTemperature/heatIndex');
 const math = require('mathjs');
 
 //const test = { t: 0, windSpeed: 20 };
@@ -6,35 +7,41 @@ const math = require('mathjs');
 test('Calculate heat index 1', () => {
     const temp = 80;
     const humidity = 70;
-    expect(math.round(calculateHeatIndex(temp, humidity), 0)).toBe(83);
+    const obj = { temperature: temp, relativeHumidity: humidity };
+    expect(math.round(calculateHeatIndex(obj), 0)).toBe(83);
 });
 
 test('Calculate heat index 2', () => {
     const temp = 90;
     const humidity = 100;
-    expect(math.round(calculateHeatIndex(temp, humidity), 0)).toBe(132);
+    const obj = { temperature: temp, relativeHumidity: humidity };
+    expect(math.round(calculateHeatIndex(obj), 0)).toBe(132);
 });
 
 test('Calculate heat index 3', () => {
     const temp = 40;
     const humidity = 100;
-    expect(math.round(calculateHeatIndex(temp, humidity), 0)).toBe(40);
+    const obj = { temperature: temp, relativeHumidity: humidity };
+    expect(math.round(calculateHeatIndex(obj), 0)).toBe(40);
 });
 
-test('Calculate heat index 2', () => {
+test('Calculate heat index 4', () => {
     const temp = 20;
     const humidity = 100;
-    expect(math.round(calculateHeatIndex(temp, humidity), 0)).toBe(20);
+    const obj = { temperature: temp, relativeHumidity: humidity };
+    expect(math.round(calculateHeatIndex(obj), 0)).toBe(20);
 });
 
-test('Calculate heat index 2', () => {
+test('Calculate heat index 5', () => {
     const temp = 120;
     const humidity = 4;
-    expect(math.round(calculateHeatIndex(temp, humidity), 0)).toBe(109);
+    const obj = { temperature: temp, relativeHumidity: humidity };
+    expect(math.round(calculateHeatIndex(obj), 0)).toBe(109);
 });
 
-test('Calculate heat index 2', () => {
+test('Calculate heat index 6', () => {
     const temp = 80;
     const humidity = -5;
-    expect(() => calculateHeatIndex(temp, humidity)).toThrow();
+    const obj = { temperature: temp, relativeHumidity: humidity };
+    expect(() => heatIndex(obj)).toThrow();
 });
