2e14e80f6ce27aad700d371b76d826937675d40e
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-11-29 11:14:36 +0200

    Modify heatIndex calculations

--- apparentTemperature/heatIndex.js
+++ apparentTemperature/heatIndex.js
@@ -1,11 +1,12 @@
-const obj = {
-  t: 0,
-  withCelcius: false,
-  withFahrenheit: false,
-  relativeHumidity: 0,
-  returnFahrenheit: false,
-  returnCelcius: false
-};
+const convertTemperature = require('../utils/temperatureConvertion');
+// const obj = {
+//   temperature: 0,
+//   withCelcius: false,
+//   withFahrenheit: true,
+//   relativeHumidity: 0,
+//   returnFahrenheit: true,
+//   returnCelcius: false
+// };
 
 // heat index according to Rothfusz 1990
 
@@ -14,34 +15,56 @@ const obj = {
 // T in F
 // R in % (R is humidity)
 
-const calculateHeatIndex = (t, r) => {
-  if(r<0 || r > 100){
-    throw new Error('Relative humidity misy be from 0 to 100% as intergers');
-  }
-  let temporaryCalculation = t;
-  if(t <= 40){
-    return t;
-  } else if(t > 40){
-    temporaryCalculation = -10.3 + (1.1*t) + (0.047*r);
-    if(temporaryCalculation < 79){
+// heat index algorytm NWS 2011
+const calculateHeatIndex = ({ temperature, relativeHumidity }) => {
+  let temporaryCalculation = temperature;
+  if (temperature <= 40) {
+    return temperature;
+  } else if (temperature > 40) {
+    temporaryCalculation = -10.3 + (1.1 * temperature) + (0.047 * relativeHumidity);
+    if (temporaryCalculation < 79) {
       return temporaryCalculation;
-    }  else {
-      temporaryCalculation = -42.379 + (2.04901523*t) + (10.14333127*r) - (0.22475541*t*r) - ((6.83783*Math.pow(10, -3))*Math.pow(t, 2)) - ((5.481717*Math.pow(10, -2))*Math.pow(r, 2)) + ((1.22874*Math.pow(10, -3))*Math.pow(t, 2)*r) + ((8.5282*Math.pow(10, -4))*t*Math.pow(r, 2)) - ((1.99 * Math.pow(10, -6))*Math.pow(t, 2)*Math.pow(r, 2));
-      if(r <= 13 && t >= 80 && t <= 112){
-        temporaryCalculation = temporaryCalculation - ( (13-r)/4 )*Math.pow( ( (17-Math.abs(t-95))/17 ), 0.5);
-      } else if(r > 85 && t >= 80 && t <= 87){
-        temporaryCalculation = temporaryCalculation + 0.02 * (r - 85 ) * (87 - t);
-      } else{
+    } else {
+      temporaryCalculation = -42.379 + (2.04901523 * temperature) + (10.14333127 * relativeHumidity) - (0.22475541 * temperature * relativeHumidity) - ((6.83783 * Math.pow(10, -3)) * Math.pow(temperature, 2)) - ((5.481717 * Math.pow(10, -2)) * Math.pow(relativeHumidity, 2)) + ((1.22874 * Math.pow(10, -3)) * Math.pow(temperature, 2) * relativeHumidity) + ((8.5282 * Math.pow(10, -4)) * temperature * Math.pow(relativeHumidity, 2)) - ((1.99 * Math.pow(10, -6)) * Math.pow(temperature, 2) * Math.pow(relativeHumidity, 2));
+      if (relativeHumidity <= 13 && temperature >= 80 && temperature <= 112) {
+        temporaryCalculation = temporaryCalculation - ((13 - relativeHumidity) / 4) * Math.pow(((17 - Math.abs(temperature - 95)) / 17), 0.5);
+      } else if (relativeHumidity > 85 && temperature >= 80 && temperature <= 87) {
+        temporaryCalculation = temporaryCalculation + 0.02 * (relativeHumidity - 85) * (87 - temperature);
+      } else {
         return temporaryCalculation;
       }
     }
-  } 
+  }
 };
 
-// heat index algorytm NWS 2011
 
-// temperature sould be coming in F
+const heatIndex = ({ temperature, relativeHumidity, withFahrenheit = true, withCelcius = false, returnFahrenheit = true, returnCelcius = false }) => {
+  if (withFahrenheit) {
+    if (relativeHumidity < 0 || relativeHumidity > 100) {
+      throw new Error('Relative humidity misy be from 0 to 100% as intergers');
+    } else {
+      const obj = { temperature: temperature, relativeHumidity: relativeHumidity };
+      if (returnFahrenheit) {
+        return calculateHeatIndex(obj);
+      } else {
+        const tempData = { t: calculateHeatIndex(obj), fromFahrenheit: true, toCelcius: true };
+        return convertTemperature(tempData);
+      }
 
+    }
 
+  } else if (withCelcius) {
+    const tempData = { t: temperature, fromCelcius: true, toFahrenheit: true };
+    const obj = { temperature: convertTemperature(tempData), relativeHumidity: relativeHumidity };
+    if (returnFahrenheit) {
+      return calculateHeatIndex(obj);
+    } else {
+      const tempData = { t: calculateHeatIndex(obj), fromFahrenheit: true, toCelcius: true };
+      return convertTemperature(tempData);
+    }
+  }
+
+};
 
 module.exports = calculateHeatIndex;
+module.exports = heatIndex;
