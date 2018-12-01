f938880a271f065b5169da559deef001f5af867b
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-12-01 19:08:19 +0200

    Refactor dew point modules

--- test/dewPoint.test.js
+++ test/dewPoint.test.js
@@ -1,36 +1,43 @@
-const calculateDewPoint = require('../utils/dewPoint');
-const math = require('mathjs');
+const calculateDewPoint = require("../utils/dewPoint");
+const math = require("mathjs");
 
-// calculate dew point for all the table values, see table 
+// for the test values, check table: https://www.mrfixitbali.com/images/articleimages/dew-point-chart-full.pdf
 
-test('calculate dew point ', () => {
-    expect(math.round(calculateDewPoint(15, 60), 2)).toBe(7.30);
+test("calculate dew point at absolute humidity and temperature above zero ", () => {
+  expect(calculateDewPoint(5, 100)).toBe(5);
+  expect(calculateDewPoint(15, 100)).toBe(15);
+  expect(calculateDewPoint(20, 100)).toBe(20);
+  expect(calculateDewPoint(27, 100)).toBe(27);
+  expect(calculateDewPoint(30, 100)).toBe(30);
+  expect(calculateDewPoint(35.7, 100)).toBe(35.7);
+  expect(calculateDewPoint(40, 100)).toBe(40);
+  expect(calculateDewPoint(50, 100)).toBe(50);
+  expect(() => calculateDewPoint(60, 100)).toThrow(RangeError);
 });
 
-test('calculate dew point ', () => {
-    expect(math.round(calculateDewPoint(15, 55), 2)).toBe(6.03);
+test("calculate dew point at absolute humidity and temperature below zero ", () => {
+  expect(calculateDewPoint(-5, 100)).toBe(-5);
+  expect(calculateDewPoint(-20, 100)).toBe(-20);
+  expect(calculateDewPoint(-30, 100)).toBe(-30);
+  expect(calculateDewPoint(-35.7, 100)).toBe(-35.7);
+  expect(calculateDewPoint(-40, 100)).toBe(-40);
+  expect(() => calculateDewPoint(-50, 100)).toThrow(RangeError);
+  expect(() => calculateDewPoint(-60, 100)).toThrow(RangeError);
 });
 
-test('calculate dew point ', () => {
-    expect(math.round(calculateDewPoint(15, 60), 2)).toBe(7.30);
+test("calculate dew point with temperature 15C and humidity above 50%", () => {
+  expect(calculateDewPoint(15, 55)).toBe(6.03);
+  expect(calculateDewPoint(15, 60)).toBe(7.3);
+  expect(calculateDewPoint(15, 65)).toBe(8.47);
+  expect(calculateDewPoint(15, 70)).toBe(9.57);
+  expect(calculateDewPoint(15, 75)).toBe(10.6);
+  expect(calculateDewPoint(15, 90)).toBe(13.37);
+  expect(calculateDewPoint(15, 85)).toBe(12.5);
 });
 
-test('calculate dew point ', () => {
-    expect(math.round(calculateDewPoint(15, 65), 2)).toBe(8.47);
-});
-
-test('calculate dew point ', () => {
-    expect(math.round(calculateDewPoint(15, 70), 2)).toBe(9.57);
-});
-
-test('calculate dew point ', () => {
-    expect(math.round(calculateDewPoint(15, 75), 2)).toBe(10.60);
-});
-
-// outside table
-// to check calculations http://www.dpcalc.org/
-
-test('calculate dew point ', () => {
-    expect(math.round(calculateDewPoint(16, 40), 0)).toBe(2);
-    expect(math.round(calculateDewPoint(16, 40), 2)).toBe(2.4);
+test("calculate dew point with temperature 21 and humidity below 50%", () => {
+  expect(calculateDewPoint(21, 45)).toBeCloseTo(8.61, 1);
+  expect(calculateDewPoint(21, 40)).toBeCloseTo(6.88, 1);
+  expect(calculateDewPoint(21, 35)).toBeCloseTo(4.94, 1);
+  expect(calculateDewPoint(21, 30)).toBeCloseTo(2.75, 1);
 });
--- utils/dewPoint.js
+++ utils/dewPoint.js
@@ -1,49 +1,44 @@
-const math = require('mathjs');
-// td = ta - ((100 - rh) / 5)
-// where td - dewpoint temperature
-// ta - ambient temperature in C
-// rh - relative humidity in %
-// rh >= 50%
+/*
+  Dew Point Temperature
+  td - dew point in C
+  ta - dry bulb temperature in C (ambient temperature, temperature of air, temperature received from forecast)
+  and -40°C <= ta <= 50°C
+  rh - relative humidity in %
+  if rh = 100% => ta === td
+  if rh >= 50%
+  td = ta - ((100 - rh) / 5)
+  if rh  1% - 99%
+  or
+  td = ( B*( ln(rh/100) + (A*ta / (B + ta)) ) / (A - ln(rh/100) -  (A*ta / (B + ta))) )
+  AERK recommendation coefficients
+  A = 17.625
+  B = 243.04°C
 
-//const TD1 = (B1 * Math.log(Math.exp((A1 * T) / (B1 + T)) * (RH / 100))) / (A1 - Math.log(Math.exp((A1 * T) / (B1 + T)) * (RH / 100)));
-// where 
-// A = 7.625,
-// B = 243.04°C
-// and -40°C <= t <= 50°C
+*/
+const math = require("mathjs");
 
-const calculateDewPoint = (T, RH) => {
-  // calculations according to the initial formula
-  // coefficients from https://journals.ametsoc.org/doi/pdf/10.1175/BAMS-86-2-225
-  // let a = 7.625;
-  // let b = 243.04;
-  // let c = 610.94;
-  // initial formula
-  // td = ( B*( ln(rh/100) + (A*ta / (B + ta)) ) / (A - ln(rh/100) -  (A*ta / (B + ta))) )
-  const A = 7.625;
-  const B = 243.04;
-  const y = Math.exp((A * T) / (B + T)) * (RH / 100);
-  const x = Math.log(y);
-  const TD = (B * x) / (A - x);
-  const TDD = (B * (Math.log(RH / 100) + (A * T / (B + T))) / (A - Math.log(RH / 100) - (A * T / (B + T))))
+const calculateDewPoint = (ta, rh) => {
+  if (ta > 50) {
+    throw new RangeError(
+      "Temperature too big to perform accurate calculations"
+    );
+  } else if (ta < -40) {
+    throw new RangeError(
+      "Temperature too low to perform accurate calculations "
+    );
+  }
+  if (rh === 100) {
+    return ta;
+  } else {
+    const A1 = 17.625;
+    const B1 = 243.04;
 
-  // AERK recommendation coefficients
-  const A1 = 17.625;
-  const B1 = 243.04;
+    const y1 = Math.exp((A1 * ta) / (B1 + ta)) * (rh / 100);
+    const x1 = Math.log(y1);
+    const td = (B1 * x1) / (A1 - x1);
 
-  const y1 = Math.exp((A1 * T) / (B1 + T)) * (RH / 100);
-  const x1 = Math.log(y1);
-  const TD1 = (B1 * x1) / (A1 - x1);
-
-  // this if for when rh > 50%
-  // if (RH > 50) {
-  //   return T - ((100 - RH) / 5);
-  // }
-
-  return TD1;
-
-
+    return math.round(td, 2);
+  }
 };
 
 module.exports = calculateDewPoint;
-
-// http://asciimath.org/
