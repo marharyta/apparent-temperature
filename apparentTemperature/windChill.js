cb731d1e5e8dd18b19845c141951d5cc75684909
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-12-01 20:17:29 +0200

    Improve windChill calculations

--- apparentTemperature/windChill.js
+++ apparentTemperature/windChill.js
@@ -1,5 +1,11 @@
-// old formula for wind chill
+/*
+  Wind Chill and New Wind Chill
 
+  new formula for wind chill
+  NWCT = 13.12 + 0.6215*T - 11.37 * V^0.16 + 0.3965 * T * V^0.16
+  V - air speed in km/h, T <= 10 C, V >= 4.8 km/h
+  by default calculates in C and km/h
+*/
 const obj = {
   t: 0,
   withCelcius: false,
@@ -12,30 +18,46 @@ const obj = {
   returnCelcius: false
 };
 
-// new formula for wind chill
-// NWCT = 13.12 + 0.6215*T - 11.37 * V^0.16 + 0.3965 * T * V^0.16
-// V - air speed in km/h, T <= 10 C, V >= 4.8 km/h
-// by default calculates in C and km/h 
-
-
-//  WC T = 35.74 + 0.62I5T - 35.75V0 1 6+ 0.4275TV0,6
-
 const calculateNewWindChillWithCelcius = (t, v) => {
-  return 13.12 + (0.6215*t) - 11.37 * Math.pow(v, 0.16) + (0.3965 * t * Math.pow(v, 0.16));
+  return (
+    13.12 +
+    0.6215 * t -
+    11.37 * Math.pow(v, 0.16) +
+    0.3965 * t * Math.pow(v, 0.16)
+  );
 };
 
 const calculateNewWindChillWithFahrenheit = (t, v) => {
-  return 35.74 + (0.6215*t) - 35.75 * Math.pow(v, 0.16) + (0.4275 * t * Math.pow(v, 0.16));
+  return (
+    35.74 +
+    0.6215 * t -
+    35.75 * Math.pow(v, 0.16) +
+    0.4275 * t * Math.pow(v, 0.16)
+  );
 };
 
-const newWindChill = ({ t, withCelcius, withFahrenheit, windSpeed, kmH, ms, mph, returnFahrenheit, returnCelcius }) => { 
-  if(t == undefined || windSpeed == undefined){
-    throw new Error('please, input temperature and wind speed');
-  } else if (t > 10 && !withFahrenheit){
-    throw new RangeError('temperatures above 10 C are not applicable to this calculation');
-  } else if (windSpeed < 4.8){
-    throw new RangeError('wind speed below 4.8 kmH are not applicable to this calculation');
-  } else if(withFahrenheit && mph && returnFahrenheit){
+const newWindChill = ({
+  t,
+  withCelcius,
+  withFahrenheit,
+  windSpeed,
+  kmH,
+  ms,
+  mph,
+  returnFahrenheit,
+  returnCelcius
+}) => {
+  if (t == undefined || windSpeed == undefined) {
+    throw new Error("please, input temperature and wind speed");
+  } else if (t > 10 && !withFahrenheit) {
+    throw new RangeError(
+      "temperatures above 10 C are not applicable to this calculation"
+    );
+  } else if (windSpeed < 4.8) {
+    throw new RangeError(
+      "wind speed below 4.8 kmH are not applicable to this calculation"
+    );
+  } else if (withFahrenheit && mph && returnFahrenheit) {
     return calculateNewWindChillWithFahrenheit(t, windSpeed);
   } else {
     return calculateNewWindChillWithCelcius(t, windSpeed);
--- test/windChill.test.js
+++ test/windChill.test.js
@@ -1,30 +1,103 @@
-const newWindChill = require('../apparentTemperature/windChill');
-const math = require('mathjs');
+/*
+  Wind Chill and New Wind Chill
+*/
+
+const newWindChill = require("../apparentTemperature/windChill");
+const math = require("mathjs");
 
 //const test = { t: 0, windSpeed: 20 };
 
-test('Calculate wind Chill', () => {
-    const temperatureAndWind = { t: 0, windSpeed: 20 };
-    expect(math.round(newWindChill(temperatureAndWind), 10)).toBe(math.round(-5.242223279780932, 10));
+test("Calculate wind Chill", () => {
+  const temperatureAndWind = { t: 0, windSpeed: 20 };
+  expect(math.round(newWindChill(temperatureAndWind), 10)).toBe(
+    math.round(-5.242223279780932, 10)
+  );
 });
 
-test('Input empty object', () => {
-    const emptyObject = {};
-    expect(() => newWindChill(emptyObject)).toThrow();
+test("Calculate wind with table values", () => {
+  const temperatureAndWind = { t: 0, windSpeed: 20 };
+  expect(math.round(newWindChill(temperatureAndWind), 10)).toBe(
+    math.round(-5.242223279780932, 10)
+  );
+
+  const testTemp1 = {
+    t: 5,
+    withCelcius: true,
+    withFahrenheit: false,
+    windSpeed: 25,
+    kmH: true,
+    ms: false,
+    mph: false,
+    returnFahrenheit: false,
+    returnCelcius: true
+  };
+
+  expect(math.round(newWindChill(testTemp1), 0)).toBe(1);
+  const testTemp2 = {
+    t: -5,
+    withCelcius: true,
+    withFahrenheit: false,
+    windSpeed: 25,
+    kmH: true,
+    ms: false,
+    mph: false,
+    returnFahrenheit: false,
+    returnCelcius: true
+  };
+
+  expect(math.round(newWindChill(testTemp2), 0)).toBe(-12);
+
+  const testTemp3 = {
+    t: -25,
+    withCelcius: true,
+    withFahrenheit: false,
+    windSpeed: 25,
+    kmH: true,
+    ms: false,
+    mph: false,
+    returnFahrenheit: false,
+    returnCelcius: true
+  };
+
+  expect(math.round(newWindChill(testTemp3), 0)).toBe(-38);
+
+  const testTemp4 = {
+    t: -25,
+    withCelcius: true,
+    withFahrenheit: false,
+    windSpeed: 25,
+    kmH: true,
+    ms: false,
+    mph: false,
+    returnFahrenheit: false,
+    returnCelcius: true
+  };
+
+  expect(math.round(newWindChill(testTemp4), 0)).toBe(-38);
 });
 
-test('Input too high temperature', () => {
-    const hightTemperature = {t: 25, windSpeed: 20};
-    expect(() => newWindChill(hightTemperature)).toThrow(RangeError);
+test("Input empty object", () => {
+  const emptyObject = {};
+  expect(() => newWindChill(emptyObject)).toThrow();
 });
 
-test('Input too low wind speed', () => {
-    const lowWindSpeed = {t: 25, windSpeed: 2};
-    expect(() => newWindChill(lowWindSpeed)).toThrow(RangeError);
+test("Input too high temperature", () => {
+  const hightTemperature = { t: 25, windSpeed: 20 };
+  expect(() => newWindChill(hightTemperature)).toThrow(RangeError);
 });
 
+test("Input too low wind speed", () => {
+  const lowWindSpeed = { t: 25, windSpeed: 2 };
+  expect(() => newWindChill(lowWindSpeed)).toThrow(RangeError);
+});
 
-test('Calculate wind chill for Fahrenheit', () => {
-    const fahrenheit = {t: 40, windSpeed: 15, mph: true, withFahrenheit: true, returnFahrenheit: true};
-    expect(math.round(newWindChill(fahrenheit), 0)).toBe(32);
+test("Calculate wind chill for Fahrenheit", () => {
+  const fahrenheit = {
+    t: 40,
+    windSpeed: 15,
+    mph: true,
+    withFahrenheit: true,
+    returnFahrenheit: true
+  };
+  expect(math.round(newWindChill(fahrenheit), 0)).toBe(32);
 });
