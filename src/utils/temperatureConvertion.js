01ea40aeabe5ceaa0e6d65a6c3513817f501e577
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-12-12 16:13:19 +0200

    Lint temperature converstion module

--- src/utils/temperatureConvertion.js
+++ src/utils/temperatureConvertion.js
@@ -1,28 +1,29 @@
 const fromCelciusToKelvin = t => {
-    return t + 273.15;
+  return t + 273.15;
 };
 
 const fromKelvinToCelcius = t => {
-    return t - 273.15;
+  return t - 273.15;
 };
 
 const fromFahrenheitToKelvin = t => {
-    return (t + 459.67) * 5 / 9;
+  return ((t + 459.67) * 5) / 9;
 };
 
 const fromKelvinToFahrenheit = t => {
-    return t * 9 / 5 - 459.67;
+  return (t * 9) / 5 - 459.67;
 };
 
 const fromCelciusToFahrenheit = t => {
-    return t * 1.8 + 32;
+  return t * 1.8 + 32;
 };
 
 const fromFahrenheitToCelcius = t => {
-    return (t - 32) / 1.8;
+  return (t - 32) / 1.8;
 };
 
-//example
+/*
+example
 const obj = {
     t: 0,
     fromCelcius: false,
@@ -32,42 +33,50 @@ const obj = {
     toFahrenheit: false,
     toKelvin: false
 };
+*/
 
-const convertTemperature = ({ t, fromCelcius, fromFahrenheit, fromKelvin, toCelcius, toFahrenheit, toKelvin }) => {
-    if (fromCelcius) {
-        if (toCelcius) {
-            return t;
-        } else if (toFahrenheit) {
-            return fromCelciusToFahrenheit(t);
-        } else if (toKelvin) {
-            return fromCelciusToKelvin(t);
-        } else {
-            return t;
-        }
-    } else if (fromFahrenheit) {
-        if (toFahrenheit) {
-            return t;
-        } else if (toCelcius) {
-            return fromFahrenheitToCelcius(t);
-        } else if (toKelvin) {
-            return fromFahrenheitToKelvin(t);
-        } else {
-            return t;
-        }
-    } else if (fromKelvin) {
-        if (toKelvin) {
-            return t;
-        } else if (toCelcius) {
-            return fromKelvinToCelcius(t);
-        } else if (toFahrenheit) {
-            return fromKelvinToFahrenheit(t);
-        } else {
-            return t;
-        }
+const convertTemperature = ({
+  t,
+  fromCelcius,
+  fromFahrenheit,
+  fromKelvin,
+  toCelcius,
+  toFahrenheit,
+  toKelvin
+}) => {
+  if (fromCelcius) {
+    if (toCelcius) {
+      return t;
+    } else if (toFahrenheit) {
+      return fromCelciusToFahrenheit(t);
+    } else if (toKelvin) {
+      return fromCelciusToKelvin(t);
     } else {
-        return t;
+      return t;
     }
-
+  } else if (fromFahrenheit) {
+    if (toFahrenheit) {
+      return t;
+    } else if (toCelcius) {
+      return fromFahrenheitToCelcius(t);
+    } else if (toKelvin) {
+      return fromFahrenheitToKelvin(t);
+    } else {
+      return t;
+    }
+  } else if (fromKelvin) {
+    if (toKelvin) {
+      return t;
+    } else if (toCelcius) {
+      return fromKelvinToCelcius(t);
+    } else if (toFahrenheit) {
+      return fromKelvinToFahrenheit(t);
+    } else {
+      return t;
+    }
+  } else {
+    return t;
+  }
 };
 
 module.exports = convertTemperature;
