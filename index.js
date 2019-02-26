207edb3410cb2e4e56d9ef3e198212f3982f3619
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2019-02-26 13:08:09 +0200

    Fix api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var calculateHumidex = require("./src/apparentTemperature/humidex");
 
 //All the variable we want to expose outside needs to be property of "exports" object.
 
-exports.apparent = {
+module.exports = {
   humidex: calculateHumidex,
   heatIndex: heatIndex,
   newWindChill: newWindChill,
